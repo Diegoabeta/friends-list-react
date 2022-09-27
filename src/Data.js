0e30f5cdc4bc5460da0d5ad974fbd5487f49a239
Diego Betancourt <diegobc04@gmail.com>
2022-09-27 13:21:41 -0500

    List

---
+++ src/Data.js
@@ -0,0 +1,39 @@
+
+export const data = [
+    {
+        name: "Queen Elizabeth II",
+        age: 96,
+        photo: "https://cdn.britannica.com/06/235406-050-AB7DE276/Queen-Elizabeth-II-Buckingham-Palace-Platinum-Jubilee-London-England-June-5-2022.jpg",
+        id: 1
+
+    },
+    {
+        name: "Freddy Krueger",
+        age: 26,
+        photo: "https://upload.wikimedia.org/wikipedia/en/e/eb/Freddy_Krueger_%28Robert_Englund%29.jpg",
+        id: 2,
+    },
+    {
+        name: "Bob Marley",
+        age: 36,
+        photo: "https://www.prensalibre.com/wp-content/uploads/2018/12/f66463f4-479f-4803-8994-52d7aed05ded.jpg?quality=52&w=760&h=430&crop=1",
+        id: 3,
+
+    },
+    {
+        name: "Israel Kamakawiwoʻole",
+        age: 38,
+        photo: "https://i.ytimg.com/vi/V1bFr2SWP1I/hqdefault.jpg",
+        id: 4,
+
+    },
+    {
+        name: "Juan Gabriel",
+        age: 66,
+        photo: "https://cdn.wegow.com/media/artists/juan-gabriel/juan-gabriel-1571129253.45.2560x1440.jpg",
+        id: 5,
+    }
+
+
+]
+
---
+++ src/components/Birthdays/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './index.css'
+import { data } from '/Users/diegobetancourt/Documents/Microsoft User Data/react/friends-list-react/src/Data.js';
+
+
+export const Birthdays = () => {
+    const [people, setPeople] = React.useState(data);
+    const unfriend = (id) => { setPeople(people.filter(person => person.id !== id)) };
+    return (
+        <div className="header">
+            <h2>{people.length} Birthdays today</h2>
+
+            {people.map(({ name, age, photo, id }) => {
+
+                return (
+                    <div key={id}>
+                        <div className="flex1">
+                            <div className="flexphoto">
+                                <img src={photo} alt="foto" />
+                            </div>
+                            <div className="flex2">
+                                <h3>{name}</h3>
+                                <h4>{age} years</h4>
+                            </div>
+                            <button onClick={() => unfriend(id)}>unfriend</button>
+
+                        </div>
+
+                    </div>
+
+                );
+            })}
+            <button className="unfriendAll" onClick={() => setPeople([])}>Unfriend All</button>
+        </div>
+
+
+    )
+}
+
+
